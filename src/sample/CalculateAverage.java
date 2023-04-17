cf4bae6ce58eb6a0e71879a70da26407950cfc74
sr0427 <220029re@yse-c.net>
2023-04-17 12:17:51 +0900

    add

---
+++ src/sample/CalculateAverage.java
@@ -0,0 +1,38 @@
+package sample;
+
+import java.util.Scanner;
+
+
+
+public class CalculateAverage {
+    
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input score of Japanese!");
+        int Japanese = sc.nextInt();
+
+        System.out.println("Input score of mathmatic!");
+        int mathmatic = sc.nextInt();
+
+        System.out.println("Input score of Science!");
+        int Science = sc.nextInt();
+
+        System.out.println("Input score of Social!");
+        int Social = sc.nextInt();
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/sample/ItemCaIcuIate.java
@@ -0,0 +1,67 @@
+package sample;
+
+public class ItemCalculate {
+    public static void main(String[] args) {
+        String name = "コーヒー";
+        int price = 100;
+        int amount = 5;
+        boolean isSale = true;
+
+        int total_price = price * amount;
+
+        System.out.println(name);
+        System.out.println(total_price);
+        System.out.println(isSale);
+
+         //Complie Error
+        //total_price = name * amounr;
+
+        //comsuption tax
+        final float TAX_RATE = 0.1f;
+
+        System.out.println(TAX_RATE);
+        //TAX_RATE = 0.8f;
+
+        price = 200;
+        float pointRate = 0.05f;
+        float point = (float) price * pointRate;
+        System.out.println(("ポイント:")+ point + "pt");
+        amount = 5;
+         //increment
+        amount++;
+        System.out.println(amount);
+        //decrement
+        amount--;
+        System.out.println(amount);
+
+
+        amount = 10;
+        //11 + 10
+        System.out.println(amount++ + amount++);
+
+        amount = 10;
+        //11 + 12
+        System.out.println(++amount + ++amount);
+
+         //(1) 2項演算
+        amount = 3;
+        amount = amount + 1;
+        System.out.println(amount);
+        //(2) 複合演算
+        amount = 3;
+        amount +=1;
+        System.out.println(amount);
+
+
+        price = 110;
+        amount = 3;
+        //price から10ひく
+        price -= 10;
+        //price に amount をかける
+        price *= amount;
+        System.out.println(price);
+    }
+
+
+
+}
